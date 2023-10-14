fa6d8334feca035a304d5715f5491b990d75c9bb
yukinnko490 <yk49.skrm@gmail.com>
2023-10-14 12:58:13 +0000

    修正箇所あり

--- memo.rb
+++ memo.rb
@@ -2,38 +2,33 @@ require "csv" # CSVファイルを扱うためのライブラリを読み込ん�
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
 
-#memo_type = gets.to_s ユーザーの入力値を取得し、数字へ変換しています
+memo_type = gets.to_i #ユーザーの入力値を取得し、数字へ変換しています
 
 # if文を使用して続きを作成していきましょう。
 # 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
-memo_type = gets.chomp.to_s
 
 
 
-if memo_type === "1"
-  puts "----メモ新規作成----"
+
+if memo_type == 1
   puts "ーーー拡張子を除いたファイルを入力して下さいーーーー"
-  memo_title = gets.chomp.to_s
+  memo_title = gets.chomp
   
   puts "ーーーメモ内容を入力してくださいーーー"
-  memo_text = gets.chomp.to_s
+  puts "完了したらCtrl + Dを押します"
+  memo = STDIN.read
   CSV.open("#{memo_title}.csv",'w') do |csv|
-    csv << [memo_text]
+    csv << [memo]
   end
   
-elsif memo_type === "2"
-  puts "----メモ編集----"
-  Dir.foreach('.') do |item|
-    next if item == '.' or item == '..'
-    puts item
-  end
-  puts "ーーー上記のcsvファイルから、編集したいファイルを拡張子を除いて入力してくださいーーー"
-  memo_title = gets.chomp.to_s
-  
+elsif memo_type == 2
+  puts "----既存のメモを編集します。ファイルを入力してください----"
+  memo_title = gets.chomp
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dを押します"
+  memo = STDIN.read
   CSV.open("#{memo_title}.csv",'a') do |csv|
-    puts "ーーー#{memo_title}.csvに追記しますーーー"
-    memo_add = gets.chomp.to_s
-    csv << [memo_add]
+    csv << [memo]
   end
  
 
